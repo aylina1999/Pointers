9d54d78cef47293943c6e71a6279f9145b4a7a4d
Lugov_rr81@top-academy.ru <Lugov_rr81@top-academy.ru>
2023-03-20 03:21:25 +0300

    Transfer by reference DONE

---
+++ References/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+//#define REFERENCE
+
+#ifdef REFERENCE
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int& ra = a;
+	ra += 3;
+	cout << a << endl;
+}
+#endif // REFERENCE
+
+
+void Exchange(int& a, int& b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	Exchange(a, b);
+	cout << a << "\t" << b << endl;
+}
+
+void Exchange(int& a, int& b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+	cout << &a << "\t" << &b << endl;
+}

20bf9a4690da4d244948ce3defd99f7b76fc6b7c
Lugov_rr81@top-academy.ru <Lugov_rr81@top-academy.ru>
2023-03-21 22:36:03 +0300

    Element added

---
+++ DynamicMemory/main.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	//1) Сздаём новый массив нужного размера:
+	int* buffer = new int[n + 1];
+	//2) Копируем всё содержимое исходного массива в новый:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	// 3) Удаляем исходный массив:
+	delete[] arr;
+	// 4) Подменяем адрес старого массива, адресом нового массива:
+	arr = buffer;
+	// 5) После всего этого в массиве 'arr' появляется элемент, в который можно сохранить добавляемое значение:
+	arr[n] = value;
+	// 6)  После добавления элемента в массив, количество его элементов увеличивается на 1:
+	n++;
+	Print(arr, n);
+	delete[] arr;
+	
+}
+
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//обращение к элементам массива через арифметику указателей и оператор разименования:
+		*(arr + i) = rand() % 100;
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//люращение к элементам массива через оператор индексирования:
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
